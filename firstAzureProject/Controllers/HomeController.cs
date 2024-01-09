ebb89c88cf2699663fb2d9d1b982c8ec05082be0
Rohit Deshmukh <Rohit Deshmukh@NILAM-DESHMUKH>
2024-01-09 23:30:52 +0530

    Add project files.

---
+++ firstAzureProject/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using log4net;
+using log4net.Config;
+using System.IO;
+using System.Reflection;
+using System.Web.Mvc;
+
+
+namespace firstAzureProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        ILog log;// = LogManager.GetLogger(typeof(HomeController));
+       //public static ILogger log;
+        public ActionResult Index()
+        {
+           
+            //using ILoggerFactory loggerfactory = LoggerFactory.Create(builder =>
+            //builder.AddProvider(new Log4NetProvider(logpath.ToString())));
+            //log = loggerfactory.CreateLogger(typeof(HomeController));
+            log= LogManager.GetLogger(typeof(HomeController));
+            log.Debug("Debug message");
+            log.Warn("Warn message");
+            log.Error("Error message");
+        
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
